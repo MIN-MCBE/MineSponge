2336ba746f3437a363608e884ff1205797fbc876
민 <61784655+MIN0707@users.noreply.github.com>
2021-12-03 13:10:36 +0900

    Update Mine.php
    if -> match

--- src/Mine/Mine.php
+++ src/Mine/Mine.php
@@ -25,27 +25,29 @@ class Mine extends PluginBase
     {
         $rand = mt_rand(1, 100);
         if ($rand === 100) return BlockFactory::getInstance()->get(129, 0);
-        if ($rand === 99) return BlockFactory::getInstance()->get(56, 0);
-        if ($rand <= 50) return BlockFactory::getInstance()->get(1, 0);
-        if ($rand <= 70) return BlockFactory::getInstance()->get(16, 0);
-        if ($rand <= 80) return BlockFactory::getInstance()->get(15, 0);
-        if ($rand <= 88) return BlockFactory::getInstance()->get(14, 0);
-        if ($rand <= 93) return BlockFactory::getInstance()->get(21, 0);
-        if ($rand <= 98) return BlockFactory::getInstance()->get(74, 0);
+        else if ($rand === 99) return BlockFactory::getInstance()->get(56, 0);
+        else if ($rand <= 50) return BlockFactory::getInstance()->get(1, 0);
+        else if ($rand <= 70) return BlockFactory::getInstance()->get(16, 0);
+        else if ($rand <= 80) return BlockFactory::getInstance()->get(15, 0);
+        else if ($rand <= 88) return BlockFactory::getInstance()->get(14, 0);
+        else if ($rand <= 93) return BlockFactory::getInstance()->get(21, 0);
+        else if ($rand <= 98) return BlockFactory::getInstance()->get(74, 0);
         else return BlockFactory::getInstance()->get(1,0);
     }
 
     public function getItem(int $id): Item
     {
-        if ($id == 14) return ItemFactory::getInstance()->get(266, 0);
-        if ($id == 15) return ItemFactory::getInstance()->get(265, 0);
-        if ($id == 16) return ItemFactory::getInstance()->get(263, 0);
-        if ($id == 56) return ItemFactory::getInstance()->get(264, 0);
-        if ($id == 129) return ItemFactory::getInstance()->get(388, 0);
-        if ($id == 21) return ItemFactory::getInstance()->get(351, 4, mt_rand(1, 4));
-        if ($id == 73 or $id == 74) return ItemFactory::getInstance()->get(331, 0, mt_rand(1, 4));
-        if ($id == 1) return ItemFactory::getInstance()->get(4, 0);
-        return ItemFactory::getInstance()->get($id);
+        return match ($id)
+        {
+            14 => ItemFactory::getInstance()->get(266, 0),
+            15 => ItemFactory::getInstance()->get(265, 0),
+            16 => ItemFactory::getInstance()->get(263, 0),
+            56 => ItemFactory::getInstance()->get(264, 0),
+            129 => ItemFactory::getInstance()->get(388, 0),
+            21 => ItemFactory::getInstance()->get(351, 4, mt_rand(1, 4)),
+            73, 74 => ItemFactory::getInstance()->get(331, 0, mt_rand(1, 4)),
+            1 => ItemFactory::getInstance()->get(4, 0)
+        };
     }
 
 }
